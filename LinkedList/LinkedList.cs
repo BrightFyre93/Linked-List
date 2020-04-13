a490899f691f713611e6e91a4d46c4f7a312f087
= <=>
2020-04-13 15:55:53 +0530

    Add project files.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<T>: ICollection<T>
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public LinkedListNode<T> Tail { get; private set; }
+
+        public LinkedListNode<T> Head { get; private set; }
+
+        #region Add
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new LinkedListNode<T>(value));
+        }
+        public void AddFirst(LinkedListNode<T> node)
+        {
+            LinkedListNode<T> temp = Head;
+
+            Head = node;
+            Head.Next = temp;
+
+            Count++;
+            if (Count == 1)
+            {
+                Tail = Head;
+            }
+        }
+
+        public void AddLast(T value)
+        {
+            AddLast(new LinkedListNode<T>(value));
+        }
+        public void AddLast(LinkedListNode<T> node)
+        {
+            if(Count ==0)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+            }
+            Tail = node;
+            Count++;
+        }
+        #endregion
+
+        
+        #region Remove
+        public void RemoveFirst()
+        {
+            if(Count !=0)
+            {
+                Head = Head.Next;
+                Count--;
+
+                if (Count == 0)
+                {
+                    Tail = null;
+                }
+            }
+        }
+        public void RemoveLast()
+        {
+            if(Count != 0)
+            {
+                if(Count == 1)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else
+                {
+                    LinkedListNode<T> current = Head;
+                    while(current.Next !=Tail)
+                    {
+                        current = current.Next;
+                    }
+                    current.Next = null;
+                    Tail = current;
+                }
+                Count--;
+            }
+        }
+        #endregion
+
+
+        #region ICollection
+        public int Count
+        {
+            get;
+            private set;
+        }
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+        public bool Contains(T item)
+        {
+            LinkedListNode<T> current = Head;
+            while(current.Next!=null)
+            {
+                if(current.Value.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            LinkedListNode<T> current = Head;
+            while(current!=null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+        public void Add(T value)
+        {
+            AddFirst(new LinkedListNode<T>(value));
+        }
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> previous = null;
+            LinkedListNode<T> current = Head;
+            while(current!=null)
+            {
+                if(current.Value.Equals(item))
+                {
+                    if(previous!=null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next==null)
+                        {
+                            Tail = previous;
+                        }
+                        Count--;
+                    }
+                    else
+                    {
+                        RemoveFirst();
+                    }
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+
+        }
+
+
+        public bool IsReadOnly { 
+            get 
+            { 
+                return false; 
+            }
+        }
+        #endregion
+    }
+}
---
+++ NodeChains/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeChains
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+    class Program
+    {
+        static int count = 1;
+        static void Main(string[] args)
+        {
+            Node first = new Node { Value = 3 };
+
+            Node middle = new Node { Value = 5};
+            first.Next = middle;
+
+            Node last = new Node { Value = 7 };
+            middle.Next = last;
+
+            PrintList(first);
+        }
+        static void PrintList(Node node)
+        {
+            while(node !=null)
+            {
+                Console.WriteLine($"Value {count} in list is {node.Value}");
+                node = node.Next;
+                count++;
+            }
+            Console.WriteLine("List finished!");
+            Console.ReadKey();
+        }
+    }
+}
